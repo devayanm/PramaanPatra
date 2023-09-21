9a2f5a79ac919441494a383ee47ed8eb510f5ef6
Asish Das <101035299+Asish2580@users.noreply.github.com>
2023-09-22 01:29:11 +0530

    generateAndVerifyToken

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const {User} = require('../models/auth');
+
+const protect = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer ')
+    ) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user=await User.findById(decode.id).select("-password");
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error("Not authorized,token failed");
+        }
+    }
+    if(!token){
+        res.status(401);
+        throw new Error("Not autorized, no-token");
+    }
+}
+
+module.exports=protect;
