3d676773d630566065c87eb97a41327ba55d23cd
Ashesh Mandal <116034358+asheshmandal2003@users.noreply.github.com>
2023-09-24 02:38:45 +0530

    updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const BirthCertificate = await hre.ethers.deployContract("BirthCertificate");
+  const BirthCertificate = await hre.ethers.getContractFactory(
+    "BirthCertificate"
+  );
+  const upload = await BirthCertificate.deploy();
 
-  await BirthCertificate.waitForDeployment();
+  await upload.deployed();
 
-  console.log(`Deployed to: ${BirthCertificate.target}`);
+  console.log("Library deployed to:", upload.address);
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
