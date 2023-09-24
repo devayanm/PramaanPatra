b0d2293ac41c699340b5631d71d830ecdee371d5
Devayan <devayan9689@gmail.com>
2023-09-25 02:45:00 +0530

    Modified Homepage

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './Homepage.css'
 
 function Homepage() {
@@ -10,13 +10,13 @@ function Homepage() {
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="hero-txt">
-                                    <p>
-                                        Your <span>Certificates</span>, Our <span>Assurance</span>,
-                                        Welcome to the <span>Future</span>
+                                    <p className="display-4">
+                                        Your <span className="text-success">Certificates</span>, Our <span className="text-success">Assurance</span>,
+                                        Welcome to the <span className="text-success">Future</span>
                                     </p>
                                 </div>
-                                <div className="hero-btn">
-                                    <a href="http://" className="btn btn-primary">
+                                <div className="hero-btn mt-4">
+                                    <a href="http://" className="btn btn-success btn-lg">
                                         Get Certified
                                     </a>
                                 </div>
@@ -33,12 +33,12 @@ function Homepage() {
                     <div className="container">
                         <div className="row">
                             <div className="col-md-6">
-                                <div className="about-1">
-                                    <div className="about-txt">
+                                <div className="about-1 mt-5">
+                                    <div className="about-txt p-4">
                                         <h2>Birth Certificates</h2>
                                         <p>Create your Birth Certificate here</p>
-                                        <div className="about-btn1">
-                                            <a href="/components/BirthCertificate" className="btn btn-primary">
+                                        <div className="about-btn1 mt-3">
+                                            <a href="/components/BirthCertificate" className="btn btn-success">
                                                 Birth Certificate
                                             </a>
                                         </div>
@@ -47,23 +47,23 @@ function Homepage() {
                             </div>
                             <div className="col-md-6">
                                 <div className="about-img1">
-                                    <img src="/images/about-img1.png" alt="" className="img-fluid" />
+                                    <img src="/images/about-img1.png" alt="" className="img-fluid rounded" />
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="about-img2">
-                                    <img src="/images/about-img2.png" alt="" className="img-fluid" />
+                                    <img src="/images/about-img2.png" alt="" className="img-fluid rounded" />
                                 </div>
                             </div>
                             <div className="col-md-6">
-                                <div className="about-2">
-                                    <div className="about-txt">
+                                <div className="about-2 mt-5">
+                                    <div className="about-txt p-4">
                                         <h2>Degree Certificates</h2>
                                         <p>Create your Degree Certificate here</p>
-                                        <div className="about-btn2">
-                                            <a href="/" className="btn btn-primary">
+                                        <div className="about-btn2 mt-3">
+                                            <a href="/" className="btn btn-success">
                                                 Degree Certificate
                                             </a>
                                         </div>
@@ -73,12 +73,12 @@ function Homepage() {
                         </div>
                         <div className="row">
                             <div className="col-md-6">
-                                <div className="about-3">
-                                    <div className="about-txt">
+                                <div className="about-3 mt-5">
+                                    <div className="about-txt p-4">
                                         <h2>Property Certificates</h2>
                                         <p>Create your Property Certificate here</p>
-                                        <div className="about-btn3">
-                                            <a href="/" className="btn btn-primary">
+                                        <div className="about-btn3 mt-3">
+                                            <a href="/" className="btn btn-success">
                                                 Property Certificate
                                             </a>
                                         </div>
@@ -87,7 +87,7 @@ function Homepage() {
                             </div>
                             <div className="col-md-6">
                                 <div className="about-img3">
-                                    <img src="/images/about-img3.png" alt="" className="img-fluid" />
+                                    <img src="/images/about-img3.png" alt="" className="img-fluid rounded" />
                                 </div>
                             </div>
                         </div>
@@ -98,4 +98,4 @@ function Homepage() {
     )
 }
 
-export default Homepage
+export default Homepage;
