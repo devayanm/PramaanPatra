1c6acf50a5aa9490813bbddab008977ea14d7a66
Devayan <devayan9689@gmail.com>
2024-07-22 11:05:24 +0530

    Changes

--- client/src/components/BirthCertificate.js
+++ client/src/components/BirthCertificate.js
@@ -1,14 +1,4 @@
-import {
-  Box,
-  Card,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  Button,
-  Stack,
-  Typography,
-  Divider,
-} from "@mui/material";
+import { Box, Card, FormControl, InputLabel, OutlinedInput, Button, Stack, Typography, Divider } from "@mui/material";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import axios from "axios";
@@ -16,17 +6,8 @@ import { useNavigate } from "react-router-dom";
 import Modal from "@mui/material/Modal";
 import { useEffect, useState } from "react";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+const style = { position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: 400, 
+bgcolor: "background.paper", border: "2px solid #000", boxShadow: 24, p: 4, };
 
 function BirthCertificate({ contract }) {
   const [open, setOpen] = useState(false);
