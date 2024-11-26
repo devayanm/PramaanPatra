0b9328d1bbd21d63b6b258ad1aae19077c4c6ec4
Devayan Mandal <devayan9689@gmail.com>
2024-11-27 02:27:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import { aadharDetails } from "./API/aadharDetails.js";
 import aadharRoute from "./routes/aadhar.js";
 
 mongoose
-  .connect("process.env.MONGO_URI", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
